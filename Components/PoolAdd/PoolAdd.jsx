3dc61f270f33ae0b8208293afafa07c350a7a2b0
Dhiraj Bari <82199369+dhiraj0911@users.noreply.github.com>
2023-04-30 17:09:30 +0530

    poolAdd page

--- Components/PoolAdd/PoolAdd.jsx
+++ Components/PoolAdd/PoolAdd.jsx
@@ -2,7 +2,8 @@ import React, {useState, useEffect} from 'react';
 import Image from 'next/image';
 
 import images from '../../assets';
-import Style from './PoolAdd.module.scss';
+import Style from './PoodAdd.module.css';
+
 import {Token, SearchToken} from '../../Components/index';
 
 const PoolAdd = () => {
@@ -67,7 +68,6 @@ const PoolAdd = () => {
                         />
                     </div>
                 </div>
-
                 {/* Select price range */}
                 <div className={Style.PoolAdd_box_price}>
                     {/* Left */}
@@ -95,125 +95,124 @@ const PoolAdd = () => {
                             <p>WETH</p>
                             <p>?</p>
                         </div>
-                    </div>
-                    {/* Fee */}
-                    <div className={Style.PoolAdd_box_price_left_fee}>
-                        <div className={Style.PoolAdd_box_price_left_fee_left}>
-                            <h4>Fee Teir</h4>
-                            <p>The % you will earn in fees</p>
+                        {/* Fee */}
+                        <div className={Style.PoolAdd_box_price_left_fee}>
+                            <div className={Style.PoolAdd_box_price_left_fee_left}>
+                                <h4>Fee Teir</h4>
+                                <p>The % you will earn in fees</p>
+                            </div>
+                            {openFee ? (
+                                <button onClick={() => setOpenFee(false)}>Hide</button>
+                            ): (
+                                <button onClick={() => setOpenFee(true)}>Hide</button>
+                            )}
                         </div>
-                        {openFee ? (
-                            <button onClick={() => setOpenFee(false)}>Hide</button>
-                        ): (
-                            <button onClick={() => setOpenFee(true)}>Hide</button>
-                        )}
-                    </div>
 
-                    {/* Fee List */}
-                    {openFee && (
-                        <div className={Style.PoolAdd_box_price_left_list}>
-                            {feePair.map((el, i) => (
-                                <div
-                                    className={Style.PoolAdd_box_price_left_item}
-                                    key={i + 1}
-                                    onClick={() => setActive(i + 1)}
-                                >
-                                    <div className={Style.PoolAdd_box_price_left_list_item}>
-                                        <p>{el.fee}</p>
-                                        <p>
-                                            {active == i + 1 ? (
-                                                <Image
-                                                    src={images.tick}
-                                                    alt="img"
-                                                    width={20}
-                                                    height={20}
-                                                />
-                                            ) : (
-                                                ""
-                                            )}
-                                        </p>    
+                        {/* Fee List */}
+                        {openFee && (
+                            <div className={Style.PoolAdd_box_price_left_list}>
+                                {feePair.map((el, i) => (
+                                    <div
+                                        className={Style.PoolAdd_box_price_left_item}
+                                        key={i + 1}
+                                        onClick={() => setActive(i + 1)}
+                                    >
+                                        <div className={Style.PoolAdd_box_price_left_list_item}>
+                                            <p>{el.fee}</p>
+                                            <p>
+                                                {active == i + 1 ? (
+                                                    <Image
+                                                        src={images.tick}
+                                                        alt="img"
+                                                        width={20}
+                                                        height={20}
+                                                    />
+                                                ) : (
+                                                    ""
+                                                )}
+                                            </p>    
+                                        </div>
+                                        <small>{el.info}</small>
+                                        <p className={Style.PoolAdd_box_price_left_list_item_para}>
+                                            {el.number}
+                                        </p>
                                     </div>
-                                    <small>{el.info}</small>
-                                    <p className={Style.PoolAdd_box_price_left_list_item_para}>
-                                        {el.number}
+                                ))}
+                            </div>
+                        )}
+                        {/* // Deposit amount */}
+                        <div className={Style.PoolAdd_box_deposit}>
+                            <h4>Deposit Amount</h4>
+                            <div className={Style.PoolAdd_box_deposit_box}>
+                                <input type="text" placeholder="0" />
+                                <div className={Style.PoolAdd_box_deposit_box_input}>
+                                    <p>
+                                        <small>UNI</small> Uniswap
+                                    </p>
+                                    <p className={Style.PoolAdd_box_deposit_box_input_item} >
+                                        Balance: 0.0
                                     </p>
                                 </div>
-                            ))}
-                        </div>
-                    )}
-                    {/* // Deposit amount */}
-                    <div className={Style.PoolAdd_box_deposit}>
-                        <h4>Deposit Amount</h4>
-                        <div className={Style.PoolAdd_box_deposit_box}>
-                            <input type="text" placeholder="0" />
-                            <div className={Style.PoolAdd_box_deposit_box_input}>
-                                <p>
-                                    <small>UNI</small> Uniswap
-                                </p>
-                                <p className={Style.PoolAdd_box_deposit_box_input_item} >
-                                    Balance: 0.0
-                                </p>
                             </div>
-                        </div>
-                        <div className={Style.PoolAdd_box_deposit_box}>
-                            <input type="text" placeholder="0" />
-                            <div className={Style.PoolAdd_box_deposit_box_input}>
-                                <p>
-                                    <small>ETH</small> Ether
-                                </p>
-                                <p className={Style.PoolAdd_box_deposit_box_input_item}>
-                                    Balance: 0.0
-                                </p>
+                            <div className={Style.PoolAdd_box_deposit_box}>
+                                <input type="text" placeholder="0" />
+                                <div className={Style.PoolAdd_box_deposit_box_input}>
+                                    <p>
+                                        <small>ETH</small> Ether
+                                    </p>
+                                    <p className={Style.PoolAdd_box_deposit_box_input_item}>
+                                        Balance: 0.0
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-
-                {/* Right */}
-                <div className={Style.PoolAdd_box_price_right}>
-                    <h4>Set Price Range</h4>
-                    <div className={Style.PoolAdd_box_price_right_box}>
-                        <p className={Style.PoolAdd_box_price_right_box_para}>
-                            Current Price: 41.3213 Testv4 per WETH
-                        </p>
-                        <Image src={images.wallet} alt="wallet" width={80} height={80} />
-                        <h3>You position will appear here.</h3>
-                    </div>
-
-                    {/* Price Range */}
-                    <div className={Style.PoolAdd_box_price_right_range}>
-                        <div className={Style.PoolAdd_box_price_right_range_box}>
-                            <p>Min Price</p>
-                            <p
-                                className={Style.PoolAdd_box_price_right_range_box}
-                                onClick={() => minPriceRange(e.target.innerText)}
-                            >
-                                <small>-</small> {minPrice} <small>+</small>
+                    {/* Right */}
+                    <div className={Style.PoolAdd_box_price_right}>
+                        <h4>Set Price Range</h4>
+                        <div className={Style.PoolAdd_box_price_right_box}>
+                            <p className={Style.PoolAdd_box_price_right_box_para}>
+                                Current Price: 41.3213 Testv4 per WETH
                             </p>
-                            <p>Testv4 per WETH</p>
+                            <Image src={images.wallet} alt="wallet" width={80} height={80} />
+                            <h3>You position will appear here.</h3>
                         </div>
-                        {/* max range */}
-                        <div className={Style.PoolAdd_box_price_right_range_box}>
-                            <p>Max Price</p>
-                            <p
-                                className={Style.PoolAdd_box_price_right_range_box}
-                                onClick={() => maxPriceRange(e.target.innerText)}
-                            >
-                                <small>-</small> {maxPrice} <small>+</small>
-                            </p>
-                            <p>Testv4 per WETH</p>
+
+                        {/* Price Range */}
+                        <div className={Style.PoolAdd_box_price_right_range}>
+                            <div className={Style.PoolAdd_box_price_right_range_box}>
+                                <p>Min Price</p>
+                                <p
+                                    className={Style.PoolAdd_box_price_right_range_box}
+                                    onClick={() => minPriceRange(e.target.innerText)}
+                                >
+                                    <small>-</small> {minPrice} <small>+</small>
+                                </p>
+                                <p>Testv4 per WETH</p>
+                            </div>
+                            {/* max range */}
+                            <div className={Style.PoolAdd_box_price_right_range_box}>
+                                <p>Max Price</p>
+                                <p
+                                    className={Style.PoolAdd_box_price_right_range_box}
+                                    onClick={() => maxPriceRange(e.target.innerText)}
+                                >
+                                    <small>-</small> {maxPrice} <small>+</small>
+                                </p>
+                                <p>Testv4 per WETH</p>
+                            </div>
                         </div>
-                    </div>
-                
-                    {/* Button */}
-                    <div className={Style.PoolAdd_box_price_right_button}>
-                        <button>Full Range</button>
-                    </div>
-                    <div className={Style.PoolAdd_box_price_right_amount}>
-                        <button>Enter a amount</button>
+                    
+                        {/* Button */}
+                        <div className={Style.PoolAdd_box_price_right_button}>
+                            <button>Full Range</button>
+                        </div>
+                        <div className={Style.PoolAdd_box_price_right_amount}>
+                            <button>Enter a amount</button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </div>    
             {openModel && (
                 <div className={Style.token}> 
                     <Token setOpenSetting={setOpenModel}/>
